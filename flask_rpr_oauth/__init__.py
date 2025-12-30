32581b775609f21b97f59322e00ebcd83635ca88
RPR Development <development@roleplayreality.nl>
2025-12-30 23:23:10 +0100

    chore: bump version to 1.1.2

--- flask_rpr_oauth/__init__.py
+++ flask_rpr_oauth/__init__.py
@@ -31,7 +31,7 @@ from .stateless import (
     clear_userinfo_cache,
 )
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __all__ = [
     # Session-based (original)
     "RPRAuth",
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="flask-rpr-oauth",
-    version="1.1.1",
+    version="1.1.2",
     author="Roleplay Reality",
     author_email="support@roleplayreality.nl",
     description="Flask OAuth 2.0 / OpenID Connect extensie voor Roleplay Reality Auth Server",
